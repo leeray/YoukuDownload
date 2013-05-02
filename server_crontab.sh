d8483b2a4746397fc04d6b5283cdbfd098e5cd7f
leeray <lee-ray@hotmail.com>
2013-05-02 15:08:32 +0800

    服务器运行crontab脚本，执行命令加入日志。

--- server_crontab.sh
+++ server_crontab.sh
@@ -2,16 +2,16 @@
 
 cd /opt/leeray/YoukuDownload
 
-cmd_360="python testthread.py 400000 100 360"
+cmd_360="python testthread.py 400000 100 360 > Logs/360_`date +\%Y\%m\%d`.log"
 echo $cmd_360
 
-cmd_anzhuo91="python testthread.py 400000 100 anzhuo91"
+cmd_anzhuo91="python testthread.py 400000 100 anzhuo91 > Logs/anzhuo91_`date +\%Y\%m\%d`.log"
 echo $cmd_anzhuo91
 
-cmd_anzhuoshichang="python testthread.py 400000 100 anzhuoshichang"
+cmd_anzhuoshichang="python testthread.py 400000 100 anzhuoshichang > Logs/anzhuoshichang_`date +\%Y\%m\%d`.log"
 echo $cmd_anzhuoshichang
 
-cmd_baidu="python testthread.py 400000 100 baidu"
+cmd_baidu="python testthread.py 400000 100 baidu > Logs/baidu_`date +\%Y\%m\%d`.log"
 echo $cmd_baidu
 
 `nohup $cmd_360`
