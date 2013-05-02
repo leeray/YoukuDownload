da8d83f521cdfdc4e23a0342b27df97ce05f03a3
leeray <lee-ray@hotmail.com>
2013-05-02 15:15:25 +0800

    服务器contab 脚本调试

--- server_crontab.sh
+++ server_crontab.sh
@@ -2,21 +2,21 @@
 
 cd /opt/leeray/YoukuDownload
 
-cmd_360="python testthread.py 400000 100 360 > Logs/360_`date +\%Y\%m\%d`.log"
+cmd_360="nohup python testthread.py 400000 100 360 > ./Logs/360_`date +\%Y\%m\%d`.log 2>&1 &"
 echo $cmd_360
 
-cmd_anzhuo91="python testthread.py 400000 100 anzhuo91 > Logs/anzhuo91_`date +\%Y\%m\%d`.log"
+cmd_anzhuo91="nohup python testthread.py 400000 100 anzhuo91 > ./Logs/anzhuo91_`date +\%Y\%m\%d`.log 2>&1 &"
 echo $cmd_anzhuo91
 
-cmd_anzhuoshichang="python testthread.py 400000 100 anzhuoshichang > Logs/anzhuoshichang_`date +\%Y\%m\%d`.log"
+cmd_anzhuoshichang="nohup python testthread.py 400000 100 anzhuoshichang > ./Logs/anzhuoshichang_`date +\%Y\%m\%d`.log 2>&1 &"
 echo $cmd_anzhuoshichang
 
-cmd_baidu="python testthread.py 400000 100 baidu > Logs/baidu_`date +\%Y\%m\%d`.log"
+cmd_baidu="nohup python testthread.py 400000 100 baidu > ./Logs/baidu_`date +\%Y\%m\%d`.log 2>&1 &"
 echo $cmd_baidu
 
-`nohup $cmd_360`
-`nohup $cmd_anzhuo91`
-`nohup $cmd_anzhuoshichang`
-`nohup $cmd_baidu`
+`$cmd_360`
+`$cmd_anzhuo91`
+`$cmd_anzhuoshichang`
+`$cmd_baidu`
 
 
