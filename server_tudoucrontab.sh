cc5dea34d184299372e377025b862316c06e0ff0
leeray <lee-ray@hotmail.com>
2013-05-13 17:11:19 +0800

    脚本调用增加安置、uc、机锋和应用汇

--- server_tudoucrontab.sh
+++ server_tudoucrontab.sh
@@ -14,9 +14,24 @@ echo $cmd_anzhuoshichang
 cmd_baidu="nohup python tudouthread.py 100000 100 baidu > ./Logs/tudou_baidu_`date +\%Y\%m\%d`.log 2>&1 &"
 echo $cmd_baidu
 
+cmd_jifeng="nohup python tudouthread.py 100000 100 jifeng > ./Logs/tudou_jifeng_`date +\%Y\%m\%d`.log 2>&1 &"
+echo $cmd_jifeng
+
+cmd_uc="nohup python tudouthread.py 100000 100 uc > ./Logs/tudou_uc_`date +\%Y\%m\%d`.log 2>&1 &"
+echo $cmd_uc
+
+cmd_anzhi="nohup python tudouthread.py 100000 100 anzhi > ./Logs/tudou_anzhi_`date +\%Y\%m\%d`.log 2>&1 &"
+echo $cmd_anzhi
+
+cmd_yingyonghui="nohup python tudouthread.py 100000 100 yingyonghui > ./Logs/tudou_yingyonghui_`date +\%Y\%m\%d`.log 2>&1 &"
+echo $cmd_yingyonghui
+
 `$cmd_360`
 `$cmd_anzhuo91`
 `$cmd_anzhuoshichang`
 `$cmd_baidu`
-
+`$cmd_jifeng`
+`$cmd_yingyonghui`
+`$cmd_uc`
+`$cmd_anzhi`
 
